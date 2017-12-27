b2d5800d8fbfc130f0aef54a8a8af75aabb2a4ca
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-27 15:52:02 +0800

    Update hsstest.sh

--- hsstest.sh
+++ hsstest.sh
@@ -2,21 +2,22 @@
 sudo su
 hostname hss
 
-echo -e "127.0.0.1       localhost
-127.0.1.1       hss
+#change host name
+cp /etc/hostname hostname
+mv /etc/hostname /etc/hostname_tmp
+sed 's/ubuntu/hss/' hostname > /etc/hostname
 
-# The following lines are desirable for IPv6 capable hosts
-::1     localhost ip6-localhost ip6-loopback
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
-" > /etc/host
+#change hosts 
+cp /etc/hosts hosts
+mv /etc/hosts /etc/hosts_tmp
+sed 's/ubuntu/hss/' hosts > /tmp/hosts
 
 cp hss.conf /usr/local/etc/oai
 cp hss_fd.conf /usr/local/etc/oai/freeDiameter
 cp acl.conf /usr/local/etc/oai/freeDiameter
 cp oai_db.sql /tmp
 cp create_db.sh /tmp
-./create_db.sh
-/root/openair-cn/SCRIPTS/run_hss
+#./create_db.sh
+#/root/openair-cn/SCRIPTS/run_hss
 
 

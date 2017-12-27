d808c60e846f5020db2e0e5adf46885c97e69b01
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-27 16:44:20 +0800

    Update hsstest.sh

--- hsstest.sh
+++ hsstest.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+sudo hostname hss
 sudo su
 #change host name
 cp /etc/hostname hostname
@@ -9,14 +11,13 @@ sed 's/ubuntu/hss/' hostname > /etc/hostname
 cp /etc/hosts hosts
 mv /etc/hosts /etc/hosts_tmp
 sed 's/ubuntu/hss/' hosts > /etc/hosts
-exit
 
 #change host name without reboot
-sudo hostname hss
 
-sudo cp hss.conf /usr/local/etc/oai
-sudo cp hss_fd.conf /usr/local/etc/oai/freeDiameter
-sudo cp acl.conf /usr/local/etc/oai/freeDiameter
-sudo cp HSS.service /etc/systemd/system
+
+#sudo cp hss.conf /usr/local/etc/oai
+#sudo cp hss_fd.conf /usr/local/etc/oai/freeDiameter
+#sudo cp acl.conf /usr/local/etc/oai/freeDiameter
+#sudo cp HSS.service /etc/systemd/system
 #sudo ./create_db.sh
 #sudo service HSS start

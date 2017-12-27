31f3ffce1eea17637162937635649b6432e28db9
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-27 17:52:05 +0800

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mysql -u root -plinux -e 'CREATE DATABASE IF NOT EXIST oai_db;'
+mysql -u root -plinux -e 'CREATE DATABASE IF NOT EXISTS oai_db;'
 mysql -u root -plinux oai_db < oai_db.sql

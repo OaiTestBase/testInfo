5f68e637bc98d9d8e96994432021d4cbce853898
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-27 15:16:51 +0800

    Create hsstest.sh

---
+++ hsstest.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+sudo hostname hss
+sudo echo -e "127.0.0.1       localhost
+127.0.1.1       hss
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+" > aa2
+
+cp hss.conf /usr/local/etc/oai
+cp hss_fd.conf /usr/local/etc/oai/freeDiameter
+cp acl.conf /usr/local/etc/oai/freeDiameter
+cp oai_db.sql /tmp
+cp create_db.sh /tmp
+./create_db.sh
+/root/openair-cn/SCRIPTS/run_hss
+
+

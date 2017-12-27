2a6d60d4fb6ff5302c48fb1deb7bff906cbe17b2
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-27 16:52:13 +0800

    Update hsstest.sh

--- hsstest.sh
+++ hsstest.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sudo hostname hss
 sudo su
 #change host name
 cp /etc/hostname hostname
@@ -13,7 +12,7 @@ mv /etc/hosts /etc/hosts_tmp
 sed 's/ubuntu/hss/' hosts > /etc/hosts
 
 #change host name without reboot
-
+sudo hostname hss
 
 #sudo cp hss.conf /usr/local/etc/oai
 #sudo cp hss_fd.conf /usr/local/etc/oai/freeDiameter

db596dc52b19c3612bd61d57817c5cc7e62ef52d
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-28 15:41:44 +0800

    Update getIPformme.sh

--- getIPformme.sh
+++ getIPformme.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+screen -d -m -S anyone ping 8.8.8.8
 screen -d -m -S HSS ping $hss_Hss
 screen -d -m -S SPGW ping $spgw_S11
-echo "hss ip ="$hss_Hss > ipHSS.txt
-echo "spgw ip ="$spgw_S11 > ipspgw.txt
+screen -d -m -S anytwo ping 9.9.9.9
+#echo "hss ip ="$hss_Hss > ipHSS.txt
+#echo "spgw ip ="$spgw_S11 > ipspgw.txt

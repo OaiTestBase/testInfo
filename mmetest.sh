c4ba71d1ae4a6f77a00020dd1029a622a36f1ea2
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-28 15:03:09 +0800

    Update mmetest.sh

--- mmetest.sh
+++ mmetest.sh
@@ -2,18 +2,19 @@
 
 #test code
 cp mme.conf mme_tmp.conf
-S1-MME="10.1.4.7"
+S1_MME="10.1.4.7"
 S11="10.1.1.7"
 SPGW_S11="10.1.1.8"
+hss_HSS="10.1.2.2"
 
 
 #get interface for s11/s1
-S1Interface=ifconfig | grep -B1 "inet addr:${S1-MME}" | awk '$1!="inet" && $1!="--" {print $1}'
+S1Interface=ifconfig | grep -B1 "inet addr:${S1_MME}" | awk '$1!="inet" && $1!="--" {print $1}'
 S11Interface=ifconfig | grep -B1 "inet addr:${S11}" | awk '$1!="inet" && $1!="--" {print $1}'
 
 #mod mme.conf
 #S1 ip
-sed -i "s/__MME_S1_C_IP__/${S1-MME}/" mme.conf
+sed -i "s/__MME_S1_C_IP__/${S1_MME}/" mme.conf
 #S1 interface
 sed -i "s/__MME_S1_C_INTERFACE__/${S1Interface}/" mme.conf
 
@@ -27,5 +28,5 @@ sed -i "s/SPGW_PRIVATE_IP/${SPGW_S11}/" mme.conf
 
 #mod mme_fd.conf
 #HSS ip
-sed -i "s/HSS_PRIVATE_IP/${S1-MME}/" mme.conf
+sed -i "s/HSS_PRIVATE_IP/${hss_HSS}/" mme.conf
 

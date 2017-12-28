516ccad0cdc8b5cfd16cee87b2fe9f1817f6deb0
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-28 18:07:01 +0800

    Update server_configure_floatingIp.sh

--- server_configure_floatingIp.sh
+++ server_configure_floatingIp.sh
@@ -2,6 +2,7 @@
 
 screen -d -m -S HOPE ping 8.8.8.8
 screen -d -m -S giveup ping $server_Hss
+screen -d -m -S giveup ping $server_S11
 screen -d -m -S REALLIFE ping 9.9.9.9
 #if [ $server_shared_int_network_floatingIp ]
 #then

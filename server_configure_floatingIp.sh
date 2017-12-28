801097a4a1792dc58a7c83f48abbb471da189e60
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-28 16:17:51 +0800

    Update server_configure_floatingIp.sh

--- server_configure_floatingIp.sh
+++ server_configure_floatingIp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ $server_shared_int_network_floatingIp ]
-then
-    screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 300
-else
-    screen -d -m -S client iperf -c $server_private_floatingIp -t 300
-fi
+screen -d -m -S giveup ping $hss_Hss
+#if [ $server_shared_int_network_floatingIp ]
+#then
+#    screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 300
+#else
+#    screen -d -m -S client iperf -c $server_private_floatingIp -t 300
+#fi

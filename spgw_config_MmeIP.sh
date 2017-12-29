ecef8422e67bced8a446ab2c587a076c23666c8f
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-29 13:49:36 +0800

    Update spgw_config_MmeIP.sh

--- spgw_config_MmeIP.sh
+++ spgw_config_MmeIP.sh
@@ -11,14 +11,14 @@ S11Interface=ifconfig | grep -B1 "inet addr:${S11}" | awk '$1!="inet" && $1!="--
 
 #mod mme.conf
 #S1 ip on mme
-sed -i "s/__MME_S1_C_IP__/${S1_MME}/" mme.conf
+sudo sed -i "s/__MME_S1_C_IP__/${S1_MME}/" mme.conf
 #S1 interface on mme
-sed -i "s/__MME_S1_C_INTERFACE__/${S1Interface}/" mme.conf
+sudo sed -i "s/__MME_S1_C_INTERFACE__/${S1Interface}/" mme.conf
 
 #S11 ip on mme
-sed -i "s/__MME_S1_C_IP__/${S11}/" mme.conf
+sudo sed -i "s/__MME_S1_C_IP__/${S11}/" mme.conf
 #S11 interface on mme
-sed -i "s/__MME_S1_C_INTERFACE__/${S11Interface}/" mme.conf
+sudo sed -i "s/__MME_S1_C_INTERFACE__/${S11Interface}/" mme.conf
  
 #SPGW ip on spgw
-sed -i "s/SPGW_PRIVATE_IP/${spgw_S11}/" mme.conf
+sudo sed -i "s/SPGW_PRIVATE_IP/${spgw_S11}/" mme.conf

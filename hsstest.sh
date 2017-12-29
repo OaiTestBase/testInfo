2e152862ebc1d5aea90cc3458ce671cea14773dc
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-29 16:48:45 +0800

    Update hsstest.sh

--- hsstest.sh
+++ hsstest.sh
@@ -6,7 +6,7 @@ sed -i "s/ubuntu/$hostname/" /etc/hosts
 #change hosts 
 sudo cp /etc/hosts hosts
 sudo cp /etc/hosts /etc/hosts_tmp
-sudo sed -i 's/$hostname/hss/' /etc/hosts
+sed -i 's/$hostname/hss/' /etc/hosts
 
 
 #change host name without reboot
@@ -18,9 +18,9 @@ ORGHNAME=`cat /etc/hostname`
 echo "sed -i 's/$ORGHNAME/hss/' /etc/hostname" > mod_hostname.sh
 sudo sh mod_hostname.sh
 
-sudo cp hss.conf /usr/local/etc/oai
-sudo cp hss_fd.conf /usr/local/etc/oai/freeDiameter
-sudo cp acl.conf /usr/local/etc/oai/freeDiameter
-sudo cp HSS.service /etc/systemd/system
+sudo cp /opt/openbaton/scripts/hss.conf /usr/local/etc/oai
+sudo cp /opt/openbaton/scripts/hss_fd.conf /usr/local/etc/oai/freeDiameter
+sudo cp /opt/openbaton/scripts/acl.conf /usr/local/etc/oai/freeDiameter
+sudo cp /opt/openbaton/scripts/HSS.service /etc/systemd/system
 sudo sh create_db.sh
 sudo service HSS start

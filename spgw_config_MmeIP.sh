f22a4fdfe3e2c16cdcf5b2727e80622e972d199a
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-29 13:47:53 +0800

    Update spgw_config_MmeIP.sh

--- spgw_config_MmeIP.sh
+++ spgw_config_MmeIP.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # config hss ip for mme
+#test code
+S1_MME="10.1.4.32"
+S11="10.1.1.34"
+spgw_S11="10.1.1.35"
+
 #get interface for s11/s1
 S1Interface=ifconfig | grep -B1 "inet addr:${S1_MME}" | awk '$1!="inet" && $1!="--" {print $1}'
 S11Interface=ifconfig | grep -B1 "inet addr:${S11}" | awk '$1!="inet" && $1!="--" {print $1}'

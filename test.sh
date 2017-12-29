94a91e0e1cc764f5a65e1cb5d85a4e0bd56cd4f4
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-29 14:20:08 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-result=`ifconfig | grep -B1 'inet addr:192.168.100.14' | awk '$1!="inet" && $1!="--" {print $1}'`
+result=`ifconfig | grep -B1 'inet addr:10.1.4.32' | awk '$1!="inet" && $1!="--" {print $1}'`
 echo "123#$result#456"
 sh test2.sh ${result}
 exit 0

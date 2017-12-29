85bfa1767654bbeedec91bf474eb9b7f59880245
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2017-12-29 14:58:51 +0800

    Update spgw_config_MmeIP.sh

--- spgw_config_MmeIP.sh
+++ spgw_config_MmeIP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # config hss ip for mme
 #test code
-sudo sed -i "s/ubuntu/$hostname/" /etc/hosts
+sed -i "s/ubuntu/$hostname/" /etc/hosts
 
 #get interface for s11/s1
 S1Interface=`ifconfig | grep -B1 "inet addr:${S1_MME}" | awk '$1!="inet" && $1!="--" {print $1}'`
@@ -11,14 +11,14 @@ echo "s1 intf = ${S1Interface}"
 echo "s11 intf = ${S11Interface}"
 #mod mme.conf
 #S1 ip on mme
-sudo sed -i "s/__MME_S1_C_IP__/${S1_MME}/" mme.conf
+sed -i "s/__MME_S1_C_IP__/${S1_MME}/" mme.conf
 #S1 interface on mme
-sudo sed -i "s/__MME_S1_C_INTERFACE__/${S1Interface}/" mme.conf
+sed -i "s/__MME_S1_C_INTERFACE__/${S1Interface}/" mme.conf
 
 #S11 ip on mme
-sudo sed -i "s/__MME_S11_C_IP__/${S11}/" mme.conf
+sed -i "s/__MME_S11_C_IP__/${S11}/" mme.conf
 #S11 interface on mme
-sudo sed -i "s/__MME_S11_C_INTERFACE__/${S11Interface}/" mme.conf
+sed -i "s/__MME_S11_C_INTERFACE__/${S11Interface}/" mme.conf
  
 #SPGW ip on spgw
-sudo sed -i "s/SPGW_PRIVATE_IP/${spgw_S11}/" mme.conf
+sed -i "s/SPGW_PRIVATE_IP/${spgw_S11}/" mme.conf

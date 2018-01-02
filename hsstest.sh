c3e442b68630072266d54882de9d4bb33f69beb4
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-01-02 14:23:30 +0800

    Update hsstest.sh

--- hsstest.sh
+++ hsstest.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 #change host name
-#sed -i "s/ubuntu/$hostname/" /etc/hosts
-#sed -i 's/$hostname/hss/' /etc/hosts
 ORGHNAME=`cat /etc/hostname`
+sed -i "s/ubuntu/$ORGHNAME/" /etc/hosts
 sed -i "s/$ORGHNAME/hss/" /etc/hosts
 sed -i "s/$ORGHNAME/hss/" /etc/hostname
 hostname hss
 
+#start HSS
 sudo cp /opt/openbaton/scripts/hss.conf /usr/local/etc/oai
 sudo cp /opt/openbaton/scripts/hss_fd.conf /usr/local/etc/oai/freeDiameter
 sudo cp /opt/openbaton/scripts/acl.conf /usr/local/etc/oai/freeDiameter

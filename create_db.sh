42252c002b95f115b76061f8ce19b6972b3a5564
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-01-02 16:48:06 +0800

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pass=`echo 'linux'` 
-
+echo $pass > 111111111.txt
 mysql -u root -p"$pass" -e 'CREATE DATABASE IF NOT EXISTS oai_db;'
+echo $pass > 222222222.txt
 mysql -u root -p"$pass" oai_db < /opt/openbaton/scripts/oai_db.sql
-echo $pass > 123456.txt
+echo $pass > 333333333.txt

a59147665fd8fab40be0e2e7f43d3797163cde1c
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-01-03 14:24:47 +0800

    Update hss_config_Mme.sh

--- hss_config_Mme.sh
+++ hss_config_Mme.sh
@@ -3,3 +3,26 @@
 #mod mme.conf
 #S1 ip on mme
 sudo sed -i "s/__HSS_PRIVATE_IP__/${hss_Hss}/" /opt/openbaton/scripts/mme_fd.conf
+
+#change host name
+sed -i "s/$ORGHNAME/nano/" /etc/hosts
+sed -i "s/$ORGHNAME/nano/" /etc/hostname
+hostname nano
+
+#start MME service
+while :
+do
+    ResultForHss3868=`nmap -p 3868 ${hss_Hss} | grep open`
+    ResultForHss5868=`nmap -p 5868 ${hss_Hss} | grep open`
+    if [ "X$ResultForHss3868" = "X" ] || [ "X$ResultForHss5868" = "X" ]; then
+        echo "wait for HSS"
+    else
+        echo "HSS is alread up"
+        break
+    fi
+    echo "\n still in loop and sleep 1 sec ~"
+    sleep 1
+done
+echo "done" > /opt/openbaton/scripts/mme_YOYO.txt
+
+#sudo service MME start

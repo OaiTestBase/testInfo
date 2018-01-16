a3a75288eac7fa464a1bb2b5792d39d8c38acb6e
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-01-16 17:25:54 +0800

    Update spgwtest.sh

--- spgwtest.sh
+++ spgwtest.sh
@@ -7,13 +7,13 @@ sed -i "s/$ORGHNAME/spgw/" /etc/hosts
 sed -i "s/$ORGHNAME/spgw/" /etc/hostname
 hostname spgw
 
-SGiInterface=`ifconfig | grep -B1 "inet addr:${public}" | awk '$1!="inet" && $1!="--" {print $1}'`
-S1_UInterface=`ifconfig | grep -B1 "inet addr:${public}" | awk '$1!="inet" && $1!="--" {print $1}'`
+SGiInterface=`ifconfig | grep -B1 "inet addr:${SGi}" | awk '$1!="inet" && $1!="--" {print $1}'`
+S1_UInterface=`ifconfig | grep -B1 "inet addr:${S1_U}" | awk '$1!="inet" && $1!="--" {print $1}'`
 S11Interface=`ifconfig | grep -B1 "inet addr:${S11}" | awk '$1!="inet" && $1!="--" {print $1}'`
 
 #config files
 sudo sed -i "s/__SPGW_SGi_INTERFACE__/${SGiInterface}/" /opt/openbaton/scripts/spgw.conf
-sudo sed -i "s/__SPGW_S1U_IP__/${public}/" /opt/openbaton/scripts/spgw.conf
+sudo sed -i "s/__SPGW_S1U_IP__/${S1_U}/" /opt/openbaton/scripts/spgw.conf
 sudo sed -i "s/__SPGW_S1U_INTERFACE__/${S1_UInterface}/" /opt/openbaton/scripts/spgw.conf
 sudo sed -i "s/__SPGW_S11_IP__/${S11}/" /opt/openbaton/scripts/spgw.conf
 sudo sed -i "s/__SPGW_S11_INTERFACE__/${S11Interface}/" /opt/openbaton/scripts/spgw.conf
@@ -23,4 +23,4 @@ sudo cp /opt/openbaton/scripts/spgw.conf /usr/local/etc/oai
 sudo cp /opt/openbaton/scripts/SPGW.service /etc/systemd/system
 
 #run spgw
-#sudo service SPGW start
+sudo service SPGW start

25b63d24b3c314b1afbca893c8f859d199d2e185
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-01-18 15:57:09 +0800

    Update spgw_config_MmeIP2.sh

--- spgw_config_MmeIP2.sh
+++ spgw_config_MmeIP2.sh
@@ -3,12 +3,12 @@
 sed -i "s/ubuntu/$hostname/" /etc/hosts
 
 #get interface for s11/s1
-S1Interface=`ifconfig | grep -B1 "inet addr:${public}" | awk '$1!="inet" && $1!="--" {print $1}'`
+S1Interface=`ifconfig | grep -B1 "inet addr:${public_floatingIp}" | awk '$1!="inet" && $1!="--" {print $1}'`
 S11Interface=`ifconfig | grep -B1 "inet addr:${S11}" | awk '$1!="inet" && $1!="--" {print $1}'`
 
 #mod mme.conf
 #S1 ip on mme
-sudo sed -i "s/__MME_S1_C_IP__/${public}/" /opt/openbaton/scripts/mme.conf
+sudo sed -i "s/__MME_S1_C_IP__/${public_floatingIp}/" /opt/openbaton/scripts/mme.conf
 #S1 interface on mme
 sudo sed -i "s/__MME_S1_C_INTERFACE__/${S1Interface}/" /opt/openbaton/scripts/mme.conf
 

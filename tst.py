141092de96ab0569b57f21afc6421d13dee622c8
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-03-12 11:46:50 +0800

    Update tst.py

--- tst.py
+++ tst.py
@@ -28,16 +28,13 @@ import time
 import yaml
 
 
-def ssh(cmd, host, user, password,S):
+def ssh(cmd, host, user, password):
     """ Run an arbitrary command over SSH. """
 
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    if S == "1":
-        client.connect(host, port=22, username=user, password=password)
-    else:
-        k = paramiko.RSAKey.from_private_key_file('/home/k200/Downloads/wowo.pem')
-        client.connect(host, port=22, username=user, pkey =k)
+
+    client.connect(host, port=22, username=user, password=password)
 
     stdin, stdout, stderr = client.exec_command(cmd, get_pty=True)
     retcode = stdout.channel.recv_exit_status()
@@ -49,36 +46,50 @@ def ssh(cmd, host, user, password,S):
         stderr.read().decode('utf-8').strip()
     )
 
-def main():
+def main():#argv=sys.argv[1:]):
     try:
-        ssh("echo 'tst123456' > /home/k200/1239.txt","140.96.102.29", "k200", "k200user","1")
-        parser = argparse.ArgumentParser()
+        ssh("echo 'tst123456' > /home/k200/1239.txt","140.96.102.29", "k200", "k200user")
+        """parser = argparse.ArgumentParser()
         parser.add_argument("yaml_cfg_file", type=argparse.FileType('r'))
         parser.add_argument("-q", "--quiet", dest="verbose", action="store_false")
         args = parser.parse_args()
-        yaml_str = args.yaml_cfg_file.read()
-        yaml_cfg = yaml.load(yaml_str)
-        curl_fmt = 'echo "{data}" > /home/k200/result.txt'
-        result  =  curl_fmt.format(data=yaml_cfg)
-        ssh(result,"140.96.102.29", "k200", "k200user")
-        """end of test code"""
-        for index, vnfr in yaml_cfg['vnfr'].items():
-            def get_cp_ip(cp_name):
-                for cp in vnfr['connection_point']:
-                    if cp['name'].endswith(cp_name):
-                        return cp['ip_address']
-
-            if 'SPGW' in vnfr['name']:
-                vnf_type = 'SPGW'
-                mgmt_ip = vnfr['mgmt_ip_address']
-                ssh("echo 'tst123456' > /home/ubuntu/1239.txt",mgmt_ip, "ubuntu","k200user","0")
-                break
-                """service_ip = get_cp_ip('cp1')"""
+        run_dir = os.path.join(os.environ['RIFT_INSTALL'], "var/run/rift")
+        if not os.path.exists(run_dir):
+            os.makedirs(run_dir)
+        log_file = "{}/ping_pong_start_traffic-{}.log".format(run_dir, time.strftime("%Y%m%d%H%M%S"))
+        # logging.basicConfig(filename=log_file, level=logging.DEBUG)
+        logger = logging.getLogger('ping-pong-start-traffic')
+        logger.setLevel(logging.DEBUG)
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        if args.verbose:
+            ch.setLevel(logging.DEBUG)
+        else:
+            ch.setLevel(logging.INFO)
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        logger.addHandler(fh)
+        logger.addHandler(ch)"""
 
     except Exception as e:
         logger.exception("Exception in {}: {}".format(__file__, e))
         sys.exit(1)
 
+    """try:
+        logger.debug("Input file: {}".format(args.yaml_cfg_file.name))
+        yaml_str = args.yaml_cfg_file.read()
+        yaml_cfg = yaml.load(yaml_str)
+        logger.debug("Input YAML: {}".format(yaml_cfg))
+        rc = start_traffic(yaml_cfg, logger)
+        logger.info("Return code: {}".format(rc))
+        
+        sys.exit(rc)
+    except Exception as e:
+        logger.exception("Exception in {}: {}".format(__file__, e))
+        sys.exit(1)"""
 
 if __name__ == "__main__":
     main()

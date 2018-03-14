231aba18ced5ad3c671b6cbea63f13721783e5b4
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-03-14 10:27:22 +0800

    Update HSS_config.sh

--- test/HSS_config.sh
+++ test/HSS_config.sh
@@ -8,9 +8,9 @@ sudo sed -i "s/$ORGHNAME/hss/" /etc/hostname
 sudo hostname hss
 
 #start HSS
-sudo cp ~/testInfo/hss.conf /usr/local/etc/oai
-sudo cp ~/testInfo/hss_fd.conf /usr/local/etc/oai/freeDiameter
-sudo cp ~/testInfo/acl.conf /usr/local/etc/oai/freeDiameter
-sudo cp ~/testInfo/HSS.service /etc/systemd/system
-#sudo sh ~/testInfo/create_db.sh
-#sudo service HSS start
+sudo cp ~/testInfo/test/hss.conf /usr/local/etc/oai
+sudo cp ~/testInfo/test/hss_fd.conf /usr/local/etc/oai/freeDiameter
+sudo cp ~/testInfo/test/acl.conf /usr/local/etc/oai/freeDiameter
+sudo cp ~/testInfo/test/HSS.service /etc/systemd/system
+sudo sh ~/testInfo/test/create_db.sh
+sudo service HSS start

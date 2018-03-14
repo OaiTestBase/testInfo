0f97bfb80d41bbbda9699933db17791e6617f28a
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-03-14 11:17:33 +0800

    Update spgw_config.sh

--- test/spgw_config.sh
+++ test/spgw_config.sh
@@ -12,13 +12,13 @@ sudo hostname spgw
 #S3 --> spgw s11 ip
 SGiInterface=`ifconfig | grep -B1 "inet addr:$1" | awk '$1!="inet" && $1!="--" {print $1}'`
 S1_UInterface=`ifconfig | grep -B1 "inet addr:$1" | awk '$1!="inet" && $1!="--" {print $1}'`
-S11Interface=`ifconfig | grep -B1 "inet addr:$3" | awk '$1!="inet" && $1!="--" {print $1}'`
+S11Interface=`ifconfig | grep -B1 "inet addr:$2" | awk '$1!="inet" && $1!="--" {print $1}'`
 
 #config files
 sudo sed -i "s/__SPGW_SGi_INTERFACE__/${SGiInterface}/" ~/testInfo/test/spgw.conf
 sudo sed -i "s/__SPGW_S1U_IP__/$1/" ~/testInfo/test/spgw.conf
 sudo sed -i "s/__SPGW_S1U_INTERFACE__/${S1_UInterface}/" ~/testInfo/test/spgw.conf
-sudo sed -i "s/__SPGW_S11_IP__/$3/" ~/testInfo/test/spgw.conf
+sudo sed -i "s/__SPGW_S11_IP__/$2/" ~/testInfo/test/spgw.conf
 sudo sed -i "s/__SPGW_S11_INTERFACE__/${S11Interface}/" ~/testInfo/test/spgw.conf
 
 #copy files

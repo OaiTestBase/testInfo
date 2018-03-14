bf5018397d1e168c158c2c560b45630ee23c5fae
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-03-14 11:28:44 +0800

    Create MME_config.sh

---
+++ test/MME_config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#change host name
+sed -i "s/ubuntu/$hostname/" /etc/hosts
+#$1 --> public
+#$2 --> S11
+
+#get interface for s11/s1
+S1Interface=`ifconfig | grep -B1 "inet addr:$1" | awk '$1!="inet" && $1!="--" {print $1}'`
+S11Interface=`ifconfig | grep -B1 "inet addr:$2" | awk '$1!="inet" && $1!="--" {print $1}'`
+
+#mod mme.conf
+#S1 ip on mme
+sudo sed -i "s/__MME_S1_C_IP__/$1/" /opt/openbaton/scripts/OpenBaton/scripts/mme.conf
+#S1 interface on mme
+sudo sed -i "s/__MME_S1_C_INTERFACE__/${S1Interface}/" /opt/openbaton/scripts/OpenBaton/scripts/mme.conf
+
+#S11 ip on mme
+sudo sed -i "s/__MME_S11_C_IP__/$2/" /opt/openbaton/scripts/OpenBaton/scripts/mme.conf
+#S11 interface on mme
+sudo sed -i "s/__MME_S11_C_INTERFACE__/${S11Interface}/" /opt/openbaton/scripts/OpenBaton/scripts/mme.conf
+ 
+#SPGW ip on spgw
+sudo sed -i "s/SPGW_PRIVATE_IP/${spgw_S11}/" /opt/openbaton/scripts/OpenBaton/scripts/mme.conf
